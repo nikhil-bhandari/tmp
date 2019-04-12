2e5964f5b2113606d24e01c14a32fd61e890b7fc
Nikhil <nikhil.bhandari.74@gmail.com>
2019-04-12 10:48:55 +0800

    Updated modal background color

--- src/components/CoreComponents.js
+++ src/components/CoreComponents.js
@@ -119,6 +119,7 @@ export const ModalWrapper = styled.div`
   margin: auto;
   box-shadow: 0 0 10px 1px rgba(0,0,0,0.5);
   min-width: 430px;
+  background-color: #FFF;
 `;
 
 export const Modal = styled.div`
